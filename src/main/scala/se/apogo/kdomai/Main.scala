ff0d07d4af7fcbce6a314688279a7d7af435b7e5
Martin Butler <martin.butler@tomologic.com>
2017-02-10 11:42:16 +0100

    Add possibility to only join certain games and at most n games

--- src/main/scala/se/apogo/kdomai/Main.scala
+++ src/main/scala/se/apogo/kdomai/Main.scala
@@ -4,73 +4,81 @@ import org.slf4j.{Logger, LoggerFactory}
 import se.apogo.kdomai.client._
 
 object Main extends App {
-  val logger: Logger = LoggerFactory.getLogger(getClass)
 
-  val kdomApiUrl = "http://localhost:8080/" // Replace with actual kdom host
-
-  val client = new KdomClient(kdomApiUrl)
-
+  /////////////////   CONFIG   //////////////////
+  val kdomApiUrl = "http://localhost/" // Replace with actual kdom host
   val playerName: String = "Kingdumino"
+  val maxNumberOfOngoingGames: Int = 5
+  // Only join these game ids (if None: join any game)
+  val gameIdsToJoin: Option[Set[String]] = None
+  //val gameIdsToJoin: Option[Set[String]] = Some(Set("d411c211-5658-437e-a2f8-7782dbc73a5f"))
+  ///////////////// END CONFIG //////////////////
 
+  val logger: Logger = LoggerFactory.getLogger(getClass)
+  val client = new KdomClient(kdomApiUrl)
   val pollTimeMs: Int = 1000
 
   playGames(Nil)
 
-  def sleep() = Thread.sleep(pollTimeMs)
-
   def playGames(myGames: Seq[MyGame]): Unit = {
-    if (myGames.isEmpty) {
-      // We're not playing any game.
-      // Look for a game to join:
-
-      val joinedGames: Seq[MyGame] = {
-        for {
-          newGame         <- client.getNewGames().get.newGames
-          gameId           = newGame.uuid
-          playerWithToken <- client.postJoinGame(gameId, playerName)
-        } yield {
-          MyGame(gameId, playerWithToken)
-        }
-      }
-
-      sleep()
-      playGames(joinedGames)
-    }
-    else {
-      // Report any finished games:
-      val finishedGames: Seq[Game] = {
-        for {
-          myGame <- myGames
-          game   <- client.getGame(myGame.gameId)
-          if game.gameOver
-        } yield {
-          game
-        }
-      }
-
-      for (finishedGame <- finishedGames) {
-        logger.info(s"Game finished: ${finishedGame.uuid}")
-      }
-
-      val myUnfinishedGames: Seq[MyGame] = myGames.filterNot(finishedGames.contains)
-
-      // Post my turns:
+    // Report any finished games:
+    val finishedGames: Seq[Game] = {
       for {
-        myGame         <- myUnfinishedGames
-        game           <- client.getGame(myGame.gameId)
-        currentPlayer  <- game.currentPlayer
-        if currentPlayer.name == myGame.playerWithToken.name
-      } {
-        // TODO: INSERT INTELLIGENCE HERE
-        // availableMoves = client.getAvailableMoves(myGame.gameId)
-        val chosenMoveNumber: Int = 0
-        client.postMove(myGame.gameId, myGame.playerWithToken.uuid, chosenMoveNumber)
+        myGame <- myGames
+        game   <- client.getGame(myGame.gameId)
+        if game.gameOver
+      } yield {
+        game
       }
-
-      sleep()
-      playGames(myUnfinishedGames)
     }
+
+    for (finishedGame <- finishedGames) {
+      logger.info(s"Game finished: ${finishedGame.uuid}")
+    }
+
+    val myUnfinishedGames: Seq[MyGame] =
+      myGames.filterNot(myGame => finishedGames.exists(_.uuid == myGame.gameId))
+
+    // Post my turns:
+    for {
+      myGame         <- myUnfinishedGames
+      game           <- client.getGame(myGame.gameId)
+      currentPlayer  <- game.currentPlayer
+      if currentPlayer.name == myGame.playerWithToken.name
+    } {
+      // TODO: INSERT INTELLIGENCE HERE
+      // availableMoves = client.getAvailableMoves(myGame.gameId)
+      val chosenMoveNumber: Int = 0
+      client.postMove(myGame.gameId, myGame.playerWithToken.uuid, chosenMoveNumber)
+    }
+
+    // Games that can be joined by me:
+    val gamesThatCanBeJoined: Seq[NewGame] = {
+      for {
+        newGame   <- client.getNewGames().get.newGames
+        if gameIdsToJoin.forall(_.contains(newGame.uuid))
+        if !newGame.joinedPlayers.exists(_.name == playerName)
+      } yield {
+        newGame
+      }
+    }
+
+    // Look for games to join:
+    val joinedGames: Seq[MyGame] = {
+      for {
+        newGame         <- gamesThatCanBeJoined.take(maxNumberOfOngoingGames - myGames.size)
+        gameId           = newGame.uuid
+        playerWithToken <- client.postJoinGame(gameId, playerName)
+      } yield {
+        MyGame(gameId, playerWithToken)
+      }
+    }
+
+    sleep()
+    playGames(myUnfinishedGames ++ joinedGames)
   }
+
+  def sleep() = Thread.sleep(pollTimeMs)
 }
 
 case class MyGame(gameId: String, playerWithToken: PlayerWithToken)
